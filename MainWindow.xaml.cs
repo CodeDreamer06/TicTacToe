b1dcbe163ce0c57da51765cc093a27fabb7e0b55
Abhinav <codedreamer06@gmail.com>
2022-01-21 10:18:14 +0530

    Refactored validation

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -6,16 +6,16 @@ using System.Windows.Media;
 
 namespace TicTacToe
 {
-    public enum MarkType
+    public enum Mark
     {
-        Free,
+        Empty,
         Nought,
         Cross
     }
 
     public partial class MainWindow : Window
     {
-        private MarkType[] Results = {};
+        private Mark[] Results = { };
         private bool Player1Turn;
         private bool GameEnded;
 
@@ -27,22 +27,32 @@ namespace TicTacToe
 
         private void NewGame()
         {
-            Results = new MarkType[9];
+            Results = new Mark[9].Select(m => Mark.Empty).ToArray();
             Player1Turn = true;
 
-            for (var i = 0; i < Results.Length; i++)
-            {
-                Results[i] = MarkType.Free;
-            }
-
-            Container.Children.Cast<Button>().ToList().ForEach(button =>
-            {
-                button.Content = String.Empty;
-                button.Background = new SolidColorBrush(Color.FromRgb(33, 34, 43));
-                button.Foreground = Brushes.Thistle;
-            });
+            SetupButtons();
 
             GameEnded = false;
+        }
+
+        private void SetupButtons()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Button button = new Button();
+                    button.Name = $"Button{i}_{j}";
+                    button.Content = String.Empty;
+                    button.Background = new SolidColorBrush(Color.FromRgb(33, 34, 43));
+                    button.Foreground = Brushes.Thistle;
+                    button.Click += Button_Click;
+
+                    Grid.SetColumn(button, j);
+                    Grid.SetRow(button, i);
+                    Container.Children.Add(button);
+                }
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -59,12 +69,12 @@ namespace TicTacToe
 
             var index = column + (row * 3);
 
-            if (Results[index] != MarkType.Free)
+            if (Results[index] != Mark.Empty)
             {
                 return;
             }
 
-            Results[index] = Player1Turn ? MarkType.Cross : MarkType.Nought;
+            Results[index] = Player1Turn ? Mark.Cross : Mark.Nought;
             button.Content = Player1Turn ? "X" : "O";
             Player1Turn ^= true;
 
@@ -73,67 +83,47 @@ namespace TicTacToe
                 button.Foreground = Brushes.Azure;
             }
 
-            CheckForWinner();
+            GameEnded = CheckForWinner();
         }
 
-        private void CheckForWinner()
+        private bool CheckForWinner()
         {
-            if (Results[0] != MarkType.Free && (Results[0] & Results[1] & Results[2]) == Results[0])
-            {
-                GameEnded = true;
-                Button0_0.Background = Button1_0.Background = Button2_0.Background = Brushes.Green;
-            }
+            var buttons = Container.Children.Cast<Button>().ToList();
 
-            else if (Results[3] != MarkType.Free && (Results[3] & Results[4] & Results[5]) == Results[3])
+            for (int i = 0; i <= 6; i += 3)
             {
-                GameEnded = true;
-                Button0_1.Background = Button1_1.Background = Button2_1.Background = Brushes.Green;
-            }
-
-            else if (Results[6] != MarkType.Free && (Results[6] & Results[7] & Results[8]) == Results[6])
-            {
-                GameEnded = true;
-                Button0_2.Background = Button1_2.Background = Button2_2.Background = Brushes.Green;
-            }
-
-            else if (Results[0] != MarkType.Free && (Results[0] & Results[3] & Results[6]) == Results[0])
-            {
-                GameEnded = true;
-                Button0_0.Background = Button0_1.Background = Button0_2.Background = Brushes.Green;
-            }
-
-            else if (Results[1] != MarkType.Free && (Results[1] & Results[4] & Results[7]) == Results[1])
-            {
-                GameEnded = true;
-                Button1_0.Background = Button1_1.Background = Button1_2.Background = Brushes.Green;
-            }
-
-            else if (Results[2] != MarkType.Free && (Results[2] & Results[5] & Results[8]) == Results[2])
-            {
-                GameEnded = true;
-                Button2_0.Background = Button2_1.Background = Button2_2.Background = Brushes.Green;
-            }
-
-            if (Results[0] != MarkType.Free && (Results[0] & Results[4] & Results[8]) == Results[0])
-            {
-                GameEnded = true;
-                Button0_0.Background = Button1_1.Background = Button2_2.Background = Brushes.Green;
-            }
-
-            else if (Results[2] != MarkType.Free && (Results[2] & Results[4] & Results[6]) == Results[2])
-            {
-                GameEnded = true;
-                Button2_0.Background = Button1_1.Background = Button0_2.Background = Brushes.Green;
-            }
-
-            if (!Results.Any(result => result == MarkType.Free))
-            {
-                GameEnded = true;
-                Container.Children.Cast<Button>().ToList().ForEach(button =>
+                if (Results[i] != Mark.Empty && (Results[i] & Results[i + 1] & Results[i + 2]) == Results[i])
                 {
-                    button.Background = Brushes.Orange;
-                });
+                    buttons[i].Background = buttons[i + 1].Background = buttons[i + 2].Background = Brushes.Green;
+                    return true;
+                }
             }
+
+            for (int i = 0; i <= 2; i++)
+            {
+                if (Results[i] != Mark.Empty && (Results[i] & Results[i + 3] & Results[i + 6]) == Results[i])
+                {
+                    buttons[i].Background = buttons[i + 3].Background = buttons[i + 6].Background = Brushes.Green;
+                    return true;
+                }
+            }
+
+            for (int i = 2; i <= 4; i += 2)
+            {
+                if (Results[4] != Mark.Empty && (Results[4] & Results[4 - i] & Results[4 + i]) == Results[4])
+                {
+                    buttons[4].Background = buttons[4 - i].Background = buttons[4 + i].Background = Brushes.Green;
+                    return true;
+                }
+            }
+
+            if (Results.All(m => m != Mark.Empty))
+            {
+                GameEnded = true;
+                return true;
+            }
+
+            return false;
         }
     }
 }
